6e6ed90537eabfeab43c97d601590bcdad329aaa
ahrech <ahrechanova@gmail.com>
2017-12-12 22:38:47 +0200

    Fixed issue #8 and #7

--- pbc/sg/test_sel_grid.py
+++ pbc/sg/test_sel_grid.py
@@ -1,10 +1,16 @@
 import pytest
 
+from pbc.sg.sg import StartGrid, Grid
+
 
 @pytest.mark.selenium
-def test_sg_sm(start_selenium, ssh_client):
+def test_sg_sm(ssh_client):
+    grid = StartGrid(Grid(ssh_client))
+    grid.download()
+    grid.start_hub()
+    grid.add_node()
     assert len(list(ssh_client.execute('pgrep java').readlines())) == 2
 
 @pytest.mark.selenium
-def test_log_no_errors(start_selenium, ssh_client):
+def test_log_no_errors(ssh_client):
     assert len(list(ssh_client.execute('grep Error log.txt').readlines())) == 0
